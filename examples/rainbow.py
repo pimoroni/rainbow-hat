c37144c1c34b4fe4b17ae19422aa3f72c8a16de3
Phil Howard <phil@gadgetoid.com>
2016-12-06 11:28:29 +0000

    Added rainbow and touch examples

---
+++ examples/rainbow.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import colorsys
+import time
+
+import rainbowhat
+
+
+try:
+    while True:
+        for x in range(7):
+            delta = time.time() * 20
+            hue = delta + (x*10)
+            hue %= 360 # Clamp to 0-359.9r
+            hue /= 360.0 # Scale to 0.0 to 1.0
+            r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(hue, 1.0, 1.0)]
+            rainbowhat.rainbow.set_pixel(6-x, r, g, b, brightness=0.1)
+
+        rainbowhat.rainbow.show()
+        time.sleep(0.05)
+
+except KeyboardInterrupt:
+    pass
---
+++ examples/touch.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+import signal
+
+import rainbowhat
+
+
+@rainbowhat.touch.A.press()
+def touch_a(channel):
+    print("Button A touched!")
+    rainbowhat.lights.rgb(1,0,0)
+
+@rainbowhat.touch.B.press()
+def touch_b(channel):
+    print("Button B touched!")
+    rainbowhat.lights.rgb(0,1,0)
+
+@rainbowhat.touch.C.press()
+def touch_c(channel):
+    print("Button C touched!")
+    rainbowhat.lights.rgb(0,0,1)
+
+signal.pause() # Pause the main thread so it doesn't exit
