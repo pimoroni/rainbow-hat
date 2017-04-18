7cab369bae1b853bcde4f50889666387ab82fbd1
David Glaude <david.glaude@gmail.com>
2017-04-18 20:51:02 +0200

    More examples from Blinkt! ported to Rainbow HAT.
    resistor_clock.py has been manually edited as it does not use NUM_PIXELS and the 0 is on the right (will on the left for the Blinkt!)

---
+++ examples/blinkt/cpu_temp.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import math
+import time
+from subprocess import PIPE, Popen
+
+#import blinkt
+from rainbowhat import rainbow as blinkt
+
+blinkt.set_clear_on_exit()
+
+def get_cpu_temperature():
+    process = Popen(['vcgencmd', 'measure_temp'], stdout=PIPE)
+    output, _error = process.communicate()
+    output = output.decode()
+
+    pos_start = output.index('=') + 1
+    pos_end = output.rindex("'")
+
+    temp = float(output[pos_start:pos_end])
+
+    return temp
+
+def show_graph(v, r, g, b):
+    v *= blinkt.NUM_PIXELS
+    for x in range(blinkt.NUM_PIXELS):
+        if v  < 0:
+            r, g, b = 0, 0, 0
+        else:
+            r, g, b = [int(min(v,1.0) * c) for c in [r,g,b]]
+        blinkt.set_pixel(x, r, g, b)
+        v -= 1
+
+    blinkt.show()
+
+blinkt.set_brightness(0.1)
+
+while True:
+    v = get_cpu_temperature() / 100.0
+    show_graph(v, 255, 255, 255)
+    time.sleep(0.01)
