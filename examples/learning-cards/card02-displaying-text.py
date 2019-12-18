49e935d85953a4cdca9bc4d8d3f4be147f5b7255
Tanya Fish <tanyafxg@gmail.com>
2019-12-18 14:24:08 +0000

    Create card02-displaying-text.py
    a program to display a simple four letter word on the alphanumeric display segments

---
+++ examples/learning-cards/card02-displaying-text.py
@@ -0,0 +1,11 @@
+# code from Learning Card 02 - Rainbow HAT
+
+# import the rainbowhat module
+import rainbowhat
+
+# adds the code to prepare to send the text to the display
+# with the word to display in double quotations
+rainbowhat.display.print_str("Test")
+
+# displays the word on the rainbow HAT
+rainbowhat.display.show()
